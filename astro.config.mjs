221f1cb11903cccb2f8a45a400a81db70be83251
OnlyKnox <joshua.knox927@gmail.com>
2024-07-22 03:20:12 -0400

    landing bottom section and images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stumpyblogs.netlify.app",
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind(), icon()]
 });
